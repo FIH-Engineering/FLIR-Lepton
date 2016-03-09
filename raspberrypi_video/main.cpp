1e052b5e5f80ee52d99f0d9b7d862c719d36c638
colleenmurach <cmurach.student@manhattan.edu>
2016-03-09 16:41:52 -0500

    window size

--- raspberrypi_video/main.cpp
+++ raspberrypi_video/main.cpp
@@ -22,7 +22,7 @@ int main( int argc, char **argv )
 	QApplication a( argc, argv );
 	
 	QWidget *myWidget = new QWidget;
-	myWidget->setGeometry(0, 20, 1000, 937);
+	myWidget->setGeometry(0, 20, 1000, 940);
 
 	//create an image placeholder for myLabel
 	//fill the top left corner with red, just bcuz
