fe6df3e653e2ddea3fcbef5730731b474dd4ba54
argitlitz <agitlitz.student@manhattan.edu>
2016-03-09 15:06:05 -0500

    mn

--- GPSD/testcode.py
+++ GPSD/testcode.py
@@ -11,9 +11,9 @@ while True:
     	# To see all report data, uncomment the line below
     	# print report
 			
-		if report['class'] == 'TPV':
-			if hasattr(report, 'time'):
-				print report.time
+	if report['class'] == 'TPV':
+	    if hasattr(report, 'time'):
+			print report.time
         
 	except KeyError:
     	pass
