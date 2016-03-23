c1c14a98288afea00e626da05c307786673ab88f
colleenmurach <cmurach.student@manhattan.edu>
2016-03-23 15:16:28 -0400

    added max temp display

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -37,7 +37,7 @@ MainWindow::MainWindow(QWidget *parent)
 	//maybe
 	QLabel *label = new QLabel(this);
 	label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
-	label->setText("first line\nsecond line");
+	label->setText("first line\nsecond" maxTemp);
 	label->setAlignment(Qt::AlignBottom | Qt::AlignRight);
 	//end maybe
 	
