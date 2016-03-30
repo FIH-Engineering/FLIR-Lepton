a47045e2859596a6cd7c8512a2bf78b3a98792e4
colleenmurach <cmurach.student@manhattan.edu>
2016-03-30 15:15:54 -0400

    nfofosdf

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -35,10 +35,13 @@ MainWindow::MainWindow(QWidget *parent)
     thread = new LeptonThread();
 	
 	//added	
-	layout->addWidget(minlabel, 2, 1);
-	layout->addWidget(maxlabel, 3, 5);
+	
 	maxlabel = new QLabel(this);
 	minlabel = new QLabel(this);
+	
+	layout->addWidget(minlabel, 2, 1);
+	layout->addWidget(maxlabel, 3, 5);
+	
 	maxlabel->setText(QString::number(maxTemp));
 	minlabel->setText(QString::number(minTemp));
 	// done adding
