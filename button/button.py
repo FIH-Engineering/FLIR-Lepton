be255eb794f956fe2a5d65ee653d007d76f68ac9
afranceschina <andrew.franceschina@gmail.com>
2016-03-30 16:53:07 -0400

    BUTTON BUTTON

--- button/button.py
+++ button/button.py
@@ -8,11 +8,10 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     red_state = GPIO.input(18)
+	blue_state = GPIO.input(17)
     if red_state == False:
         print('RED')
         time.sleep(0.2)
-	else:
-		blue_state = GPIO.input(17)
-		if blue_state == False:
-			print('BLUE')
-			time.sleep(0.2)
+	elif blue_state == False:
+		print('BLUE')
+		time.sleep(0.2)
