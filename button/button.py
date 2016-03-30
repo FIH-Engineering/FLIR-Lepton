1e53e7ee7a9662190619ccd51aa487bcb0b096c9
afranceschina <andrew.franceschina@gmail.com>
2016-03-30 16:47:42 -0400

    mas button code

--- button/button.py
+++ button/button.py
@@ -4,9 +4,14 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(18)
-    if input_state == False:
-        print('Button Pressed')
+    red_state = GPIO.input(18)
+    if red_state == False:
+        print('RED')
+        time.sleep(0.2)
+	blue_state = GPIO.input(17)
+    if blue_state == False:
+        print('BLUE')
         time.sleep(0.2)
