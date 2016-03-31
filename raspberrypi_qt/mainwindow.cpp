2f286b177e52c98eea08d21047e034a4a881fff7
e-george <egeorgiadis.student@manhattan.edu>
2016-03-31 13:08:54 -0400

    Update mainwindow.cpp

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -130,7 +130,7 @@ QSpacerItem* horizontalSpacer = new QSpacerItem(500, 0, QSizePolicy::Minimum, QS
 msgBox.setInformativeText(QString("Photo Saved as: rgb%1.jpg").arg(snapshotCount));
 QGridLayout* layout = (QGridLayout*)msgBox.layout();
 layout->addItem(horizontalSpacer, layout->rowCount(), 0, 1, layout->columnCount());
-msgBox.setStandardButtons(QMessageBox::Ok).hide(); 
+msgBox.setStandardButtons(QMessageBox::Ok).setVisible(false); 
 msgBox.button(QMessageBox::Ok)->animateClick(2000); 
 msgBox.exec();	
 }
