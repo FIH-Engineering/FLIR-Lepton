4ae793c588c359821e102f2b385a6cab19870a16
argitlitz <agitlitz.student@manhattan.edu>
2016-04-06 15:52:45 -0400

    source files
    testing multiple terminals

---
+++ Log/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+//#include<python.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "cout example" << endl;
+	printf("printf example \n");
+	
+	return 0;
+}
---
+++ Log/Source2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+//#include<python.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "cout example2" << endl;
+	printf("printf example2 \n");
+	
+	return 0;
+}
