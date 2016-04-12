ddf427f5819803136eb06375ea7afe12e55cf3e5
argitlitz <agitlitz.student@manhattan.edu>
2016-04-12 10:20:44 -0400

    .2

--- GPSD/gpsdData3.3.py
+++ GPSD/gpsdData3.3.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
         time.sleep(.2) #delay
       elif button == True:
         print("variables are resetting")
-        time.sleep(1) 
+        time.sleep(.2) 
  
   except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
     print("\nKilling Thread...")
