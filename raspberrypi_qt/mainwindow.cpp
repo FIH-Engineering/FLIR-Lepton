398a1a65351498a76eca15d5eca5174a61d4ce43
colleenmurach <cmurach.student@manhattan.edu>
2016-04-13 15:24:03 -0400

    who

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -80,7 +80,7 @@ MainWindow::MainWindow(QWidget *parent)
 	
 	if (rpiGpio.readPin(17) == mmapGpio::LOW){
 	qDebug()<<"pressed";
-	snapshotButton->clicked();
+	snapshotButton->clicked(true);
 	connect(snapshotButton, SIGNAL(clicked()), this, SLOT(saveSnapshot()));
 	}
 
