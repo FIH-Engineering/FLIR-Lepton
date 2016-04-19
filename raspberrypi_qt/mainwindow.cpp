d531f2bf40b7e1482a7d08ce322408ac2d34568c
e-george <egeorgiadis.student@manhattan.edu>
2016-04-19 15:48:33 -0400

    Update mainwindow.cpp

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -78,7 +78,7 @@ MainWindow::MainWindow(QWidget *parent)
     //{
         //usleep(500000);  // wait for 0.5 seconds
         gpio17->getval_gpio(inputstate); //read state of GPIO17 input pin
-        //cout << "Current input pin state is " << inputstate  <<endl;
+        cout << "Current input pin state is " << inputstate  <<endl;
         if(inputstate == "0") // if input pin is at state "0" i.e. button pressed
         {
 			snapshotButton->click();
