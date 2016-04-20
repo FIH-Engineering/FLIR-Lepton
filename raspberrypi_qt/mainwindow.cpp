da350d90f8a938df90ca58ddf0b007e15e0e486c
colleenmurach <cmurach.student@manhattan.edu>
2016-04-20 15:05:35 -0400

    got rid of it

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -88,9 +88,23 @@ MainWindow::MainWindow(QWidget *parent)
 	gpio17->export_gpio();
 	gpio17->setdir_gpio("in");
 	
+	
+	
 	// Update image as IR camera pans area
 	connect(thread, SIGNAL(updateImage(unsigned short *,int,int)), this, SLOT(updateImage(unsigned short *, int,int)));
 	thread->start();
+	
+	        /*usleep(500000);  // wait for 0.5 seconds
+        gpio17->getval_gpio(inputstate); //read state of GPIO17 input pin
+        cout << "Current input pin state is " << inputstate  <<endl;
+        if(inputstate == "0") // if input pin is at state "0" i.e. button pressed
+        {
+			//snapshotButton->click();
+			//saveSnapshot();
+			cout << "picture saved" << endl;
+        }
+            else
+                cout << "input pin state is definitely UnPressed. That was just noise." <<endl;*/
    
 }
 
@@ -129,23 +143,10 @@ void MainWindow::updateImage(unsigned short *data, int minValue, int maxValue){
 	//Display temperature values on GUI
 	maxlabel->setText(QString("Max Temp: %1 ").arg(maxTemp));
 	minlabel->setText(QString("Min Temp: %1 ").arg(minTemp));
-    
-		//while(1)
-    //{
 		
 		
-        usleep(500000);  // wait for 0.5 seconds
-        gpio17->getval_gpio(inputstate); //read state of GPIO17 input pin
-        cout << "Current input pin state is " << inputstate  <<endl;
-        if(inputstate == "0") // if input pin is at state "0" i.e. button pressed
-        {
-			//snapshotButton->click();
-			//saveSnapshot();
-			cout << "picture saved" << endl;
-        }
-            else
-                cout << "input pin state is definitely UnPressed. That was just noise." <<endl;
-    //} 
+
+ 
 
 }
 
