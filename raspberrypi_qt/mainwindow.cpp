cf937f7cc12fd0148c1bdab8435a5158cad787e9
colleenmurach <cmurach.student@manhattan.edu>
2016-04-22 12:09:02 -0400

    njofndso

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -175,7 +175,7 @@ void MainWindow::saveSnapshot() {
 	qDebug()<<line;
 	snapFILE.close();*/
 	QFile snapFile(QString("snapshotNumber.txt"));
-	snapFile.open(QIODevice::Append | QIODevice::ReadWrite | QIODevice::Text);
+	snapFile.open(QIODevice::ReadWrite | QIODevice::Text);
 	QTextStream snapOut(&snapFile);
 	while(!snapOut.atEnd())
 	{
