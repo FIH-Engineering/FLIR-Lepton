5985eac17996daaf132526ce5a719da76f06f928
e-george <egeorgiadis.student@manhattan.edu>
2016-04-22 15:42:58 -0400

    further fixed syntax

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -48,7 +48,7 @@ MainWindow::MainWindow(QWidget *parent)
 	this->setStyleSheet("background-color:black;");
     layout = new QGridLayout();
     mainWidget->setLayout(layout);
-	mainWidget->setStyle(new QMacStyle());
+	mainWidget->setStyle(QMacStyle);
     imageLabel = new QLabel();
     layout->addWidget(imageLabel, 0, 0, Qt::AlignCenter);
 
