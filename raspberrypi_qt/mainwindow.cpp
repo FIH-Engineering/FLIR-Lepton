5786ab818b07a7a64a0113378bc9db4368dfc73f
e-george <egeorgiadis.student@manhattan.edu>
2016-04-22 15:48:26 -0400

    Update mainwindow.cpp

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -48,7 +48,6 @@ MainWindow::MainWindow(QWidget *parent)
 	this->setStyleSheet("background-color:black;");
     layout = new QGridLayout();
     mainWidget->setLayout(layout);
-	mainWidget->setStyle(new QMacStyle());
     imageLabel = new QLabel();
     layout->addWidget(imageLabel, 0, 0, Qt::AlignCenter);
 
