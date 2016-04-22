b68e0a7eb848436df9f41fb6036fb1840de12c97
e-george <egeorgiadis.student@manhattan.edu>
2016-04-22 15:52:07 -0400

    Update main.cpp

--- raspberrypi_qt/main.cpp
+++ raspberrypi_qt/main.cpp
@@ -12,7 +12,7 @@ int main( int argc, char **argv )
 	
     MainWindow *mainWindow = new MainWindow();
 	//added//
-	mainWidget->setStyle(new QMacStyle());
+	mainWindow->setStyle(new QMacStyle());
     mainWindow->setWindowState(Qt::WindowFullScreen); 
     mainWindow->show();
 
