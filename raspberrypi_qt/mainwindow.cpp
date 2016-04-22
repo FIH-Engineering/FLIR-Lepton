cdb201f3ed50693e9a14c806b218b9463d02efda
colleenmurach <cmurach.student@manhattan.edu>
2016-04-22 11:27:55 -0400

    fidosfns

--- raspberrypi_qt/mainwindow.cpp
+++ raspberrypi_qt/mainwindow.cpp
@@ -178,6 +178,10 @@ void MainWindow::saveSnapshot() {
 	snapFile.open(QIODevice::Append | QIODevice::ReadWrite);
 	QTextStream snapOut(&snapFile);
 	snapOut<<" Picture Name: "<<QString("rgb%1.jpg").arg(snapshotCount)<<" Min Output Temperature:"<<minOutput<<" Max Output Temperature: "<<maxOutput<<" \n";
+	while (!in.atEnd()) {
+        QString line = snapOut.readLine();
+		qDebug()<<line;
+	}
 	snapFile.close();
 
 	
